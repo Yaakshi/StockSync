e3bb6bec05fd4bc65dc93d4dcb046560e23fcbfd
Yaakshithaa M <yaakshi2k3@gmail.com>
2024-08-29 14:05:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,6 @@ if($_POST){
 
   $usernameemail = $_POST['usernameemail'];
   $password = $_POST['password'];
-  $remember_me = isset($_POST['remember_me']);
 
   $query = "SELECT * FROM users WHERE email = ? AND pwd = ?;";
   $stmt = $conn->prepare($query);
@@ -24,6 +23,7 @@ if($_POST){
   $result = $stmt->get_result();
 
   if ($result->num_rows > 0) {
+
     $allRows = $result->fetch_all(MYSQLI_ASSOC);
     $user = $allRows[0];
     
@@ -34,27 +34,13 @@ if($_POST){
     $_SESSION['profile_photo'] = $user['profile_photo'];
     $_SESSION['user_role'] = $user['user_role'];
 
-    if ($remember_me)
-    {
-      // Generate a unique token
-      $token = bin2hex(random_bytes(16));
-      $expiration = date('Y-m-d H:i:s', strtotime('+30 days')); // Set token expiration (30 days)
-      
-      // Store token in the database
-      $query = "INSERT INTO user_sessions (user_id, token, expiration) VALUES (?, ?, ?)"
-      $stmt = $conn->prepare($query);
-      $stmt->bind_param("iss", $user['id'], $token, $expiration); // Bind parameters
-      $stmt->execute(); // Execute the query
-      
-      // Store token in a secure, HttpOnly cookie
-      setcookie('remember_me', $token, time() + (30 * 24 * 60 * 60), "/", "", true, true); // 30 days expiration
-    }
-
     header('Location: dashboard.php');
   }
   else {
       $error_message = "Invalid username/email or password.";
   }
+  $stmt->close();
+  $conn->close();
 }
 ?>
 
@@ -86,12 +72,6 @@ if($_POST){
         <input name="password" id="password" type="password" required>
         <label>Enter your password</label>
       </div>
-      <br>
-      <div style="color: white; text-align: left;">
-      <input type="checkbox" id="remember_me" name="remember_me">
-      <label>Remember Me</label>
-      </div>
-      <br>
       <button type="submit">Log In</button>
       <div class="register">
         <p>Don't have an account? <a href="signup.php">Sign up</a></p>
